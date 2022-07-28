5387a8235a746d6aaa7def4c3be43432065ba3d8
viniciussalleslima <vinicius.lima248@etec.sp.gov.br>
2022-07-28 14:55:16 -0300

    2 files modified

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  constructor(public pokemonService:PokemonService) {}
+
+  ngOnInit(){
+    this.pokemonService.buscarTodosPokemons().subscribe(dados => {console.log(dados)})
+  }
+
 
 }
